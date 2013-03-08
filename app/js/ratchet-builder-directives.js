7b43b35a3f8659ad32007c8464673a92cd68a8fa
Cox Chen <cox.chen@jiepang.com>
2013-03-08 18:09:20 +0800

    Applied CodeMirror for coding area

--- app/js/ratchet-builder-directives.js
+++ app/js/ratchet-builder-directives.js
@@ -3,15 +3,11 @@ var rbDirect = angular.module("ratchet-builder-directives", []);
 rbDirect.directive("iphoneWindow", function ($compile) {
 
     var linker = function (scope, element, attrs) {
-        console.log(scope);
-        console.log(element);
-        console.log(attrs);
 
         element.html(scope.content);
         $compile(element.contents())(scope);
 
-        scope.$parent.$watch("prototypeSrc", function () {
-            // console.log(scope.$parent.prototypeSrc);
+        scope.$on("update-iwindow", function (event) {
             element.html(scope.$parent.prototypeSrc);
             $compile(element.contents())(scope);
         });
--- app/js/ratchet-builder.js
+++ app/js/ratchet-builder.js
@@ -3,17 +3,85 @@ var rbApp = angular.module("ratchet-builder", ["ratchet-builder-directives"]);
 
 function PrototypeCtrl ($scope, $rootScope) {
 
-    var iphone = $('.iphone'),
-        dockingOffset = ($(window).height() + 20 + $('.docs-masthead').height() - iphone.height())/2;
-    iphone.css({top: dockingOffset});
+    var iphone = $(".iphone"),
+        dockingOffset = ($(window).height() + 20 + $(".docs-masthead").height() - iphone.height())/2;
+    iphone.css({top: Math.round(dockingOffset)});
+
+    var codeArea  = CodeMirror.fromTextArea( document.getElementById("code"), {
+            theme : "eclipse",
+            mode:  "htmlmixed",
+            indentUnit: 4,
+            lineNumbers: true
+        });
+
+    codeArea.on("change", function () {
+        setTimeout(
+            function () {
+                if ($(".cm-error").length === 0) {
+                    $scope.prototypeSrc = codeArea.getValue();
+                    $rootScope.$broadcast("update-iwindow");
+                }
+            },
+            100);
+    });
 
     $scope.prototypeSrc = [
-        "<header class='bar-title'>",
-        "    <a class='button' href='#'>Left</a>",
-        "    <h1 class='title'>Title</h1>",
-        "    <a class='button' href='#'>Right</a>",
-        "</header>"
+        "<header class=\"bar-title\">",
+        "    <a class=\"button\" href=\"#\">Left</a>",
+        "    <h1 class=\"title\">Title</h1>",
+        "    <a class=\"button\" href=\"#\">Right</a>",
+        "</header>",
+        "",
+        "<nav class=\"bar-tab\">",
+        "    <ul class=\"tab-inner\">",
+        "        <li class=\"tab-item active\">",
+        "            <a href=\"\">",
+        "                <i class=\"icon-compass\"></i>",
+        "                <div class=\"tab-label\">Nearby</div>",
+        "            </a>",
+        "        </li>",
+        "        <li class=\"tab-item\">",
+        "            <a href=\"\">",
+        "                <i class=\"icon-comment\"></i>",
+        "                <div class=\"tab-label\">Chat</div>",
+        "            </a>",
+        "        </li>",
+        "        <li class=\"tab-item\">",
+        "            <a href=\"\">",
+        "                <i class=\"icon-user\"></i>",
+        "                <div class=\"tab-label\">Profile</div>",
+        "            </a>",
+        "        </li>",
+        "    </ul>",
+        "</nav>",
+        "",
+        "<div class=\"content\">",
+        "    <ul class=\"list\">",
+        "        <li>",
+        "            <a href=\"#\">",
+        "                List item 1",
+        "                <span class=\"chevron\"></span>",
+        "            </a>",
+        "        </li>",
+        "        <li>",
+        "            <a href=\"#\">",
+        "                List item 2",
+        "                <span class=\"chevron\"></span>",
+        "            </a>",
+        "        </li>",
+        "        <li>",
+        "            <a href=\"#\">",
+        "                List item 3",
+        "                <span class=\"chevron\"></span>",
+        "            </a>",
+        "        </li>",
+        "    </ul>",
+        "</div>"
     ].join("\n");
+
+    codeArea.setValue($scope.prototypeSrc);
+
+    $(".CodeMirror").css("border", "1px solid #0d0d0d");
 
 }
 
